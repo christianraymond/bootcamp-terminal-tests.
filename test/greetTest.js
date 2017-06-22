4a5b83d8d8dfc8fe6383cba7f077392a9a381aac
christianraymond <ombenisn.raymond@gmail.com>
2017-06-22 15:07:47 +0200

    another commit

--- test/greetTest.js
+++ test/greetTest.js
@@ -5,4 +5,8 @@ describe('The greet function', function(){
     it('it should return Hello Andre!', function(){
         assert.equal(greet('Andre'), 'Hello Andre')
     });
+
+    it('should greet any other name inputed', function(){
+      assert.equal(greet('Ipreo'), 'Hello Ipreo')
+    })
 });
